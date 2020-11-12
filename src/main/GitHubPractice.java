23a968ccc639e203f799eeaa0fb34b82a1b7257c
Shirin Geldiyeva <shirin7271@gmail.com>
2020-11-12 10:46:53 -0500

    B21 is dedicated

--- src/main/GitHubPractice.java
+++ src/main/GitHubPractice.java
@@ -4,5 +4,6 @@ public class GitHubPractice {
     public static void main(String[] args) {
         System.out.println("Hello Git Last Day");
         //B21 is exited
+        //I dream beacome a tester in the future
     }
 }
